c145a75c1d98dd9512b1f7d4b889df2ce5de3016
vhqkze <zqz0601@gmail.com>
2019-03-29 22:38:34 +0800

    添加批量重命名工具，修改原有图片名称，添加README.md对批量重命名工具的描述

--- README.md
+++ README.md
@@ -5,21 +5,39 @@
 在终端中查询单词的柯林斯词典，词典数据来源为有道网页词典中的柯林斯词典。
 ### 使用
 1. 下载工程
-```sh
-git clone https://github.com/vhqkze/Python_tools.git
-```
+   ```sh
+   git clone https://github.com/vhqkze/Python_tools.git
+   ```
 2. 在`/usr/local/bin/`中创建`yd.py`的软链接
-```bash
-sudo ln -s /pathtoyourfile/Python_tools/yd.py /usr/local/bin/yd
-```
+   ```bash
+   sudo ln -s /pathtoyourfile/Python_tools/yd.py /usr/local/bin/yd
+   ```
 3. 在终端中就可以直接使用`yd vocabulary`查询单词了。
-![样例](image/2019-03-11%20231604.png)
+   ![yd](image/2019-03-11_231604.png)
 4. 与`bat`集成，在`~/.zshrc`文件末尾加上以下内容，然后使用`source ~/.zshrc`命令：
-```
-yd() {/usr/local/bin/yd "$1" | bat -p}
-```
+   ```
+   yd() {/usr/local/bin/yd "$1" | bat -p}
+   ```
 ### 依赖
 - Python 3.6+
 - Python packages:
-    - requests
-    - beautifulsoup4
+  - requests
+  - beautifulsoup4
+
+
+## pyrename.py
+使用正则表达式快速对工作目录下的所有文件及文件夹进行重命名。
+### 使用
+1. 下载工程
+   ```sh
+   git clone https://github.com/vhqkze/Python_tools.git
+   ```
+2. 在`/usr/local/bin/`中创建`yd.py`的软链接
+   ```bash
+   sudo ln -s /pathtoyourfile/Python_tools/pyrename.py /usr/local/bin/pyrename
+   ```
+3. 进入需要进行批量重命名的文件夹
+   ![pyrename](image/2019-03-29_222646.png)
+
+### 依赖
+- Python 3.6+
---
+++ pyrename.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+import re
+import os
+import sys
+
+base_dir = os.getcwd()
+
+
+# print(base_dir)
+
+
+def getfiles(dirpath):
+    # 返回目录下所有文件和目录的名称组成的list, 不包含路径
+    files = os.listdir(dirpath)
+    # files = []
+    # for item in os.listdir(dirpath):
+    #     if os.path.isfile(item):
+    #         files.append(item)
+    return files
+
+
+def rename(oldnames):
+    havefile = False
+    for filename in oldnames:
+        if re.match(pattern, filename):
+            havefile = True
+            newname = re.sub(pattern, new, filename)
+            print(filename, '  TO  ', newname)
+    if havefile:
+        exe = input('continue [y/n]?')
+        if exe == 'y':
+            for filename in oldnames:
+                if re.match(pattern, filename):
+                    newname = re.sub(pattern, new, filename)
+                    # print(filename, '  TO  ', newname)
+                    try:
+                        os.rename(filename, newname)
+                    except Exception:
+                        print('rename fail:', filename)
+        else:
+            print('rename cancel.')
+    else:
+        print('There is no files to rename.')
+    return
+
+
+def ren(namelist):
+    havefile = False
+    for oldname in namelist:
+        newname = re.sub(pattern, new, oldname)
+        if newname == oldname:
+            pass
+        else:
+            print(oldname, ' \033[31mTO\033[0m ', newname)
+            havefile = True or havefile
+    if havefile:
+        dorename = input('\033[36mrename [y/n]?\033[0m').lower()
+        if dorename == 'y':
+            for oldname in namelist:
+                newname = re.sub(pattern, new, oldname)
+                if newname == oldname:
+                    pass
+                else:
+                    try:
+                        os.rename(oldname, newname)
+                    except Exception:
+                        print('\033[33mrename fail:\033[0m', oldname)
+        else:
+            print('nothing changed, exit')
+    else:
+        print('There is no files to rename.')
+
+
+if __name__ == "__main__":
+    pattern = re.compile(sys.argv[1])
+    new = sys.argv[2]
+    filenames = getfiles(base_dir)
+    # rename(filenames)
+    ren(filenames)
