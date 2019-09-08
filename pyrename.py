a1ccc8bcedbab1728f36dc8245de1babde3b5d90
vhqkze <zqz0601@gmail.com>
2019-09-08 20:33:18 +0800

    modify pyrename.py

--- pyrename.py
+++ pyrename.py
@@ -1,79 +1,79 @@
 #!/usr/bin/env python3
-import re
+"""
+Usage:
+    test.py [-i] [-f|-d] <pattern> <repl>
+
+Arguments:
+    pattern             pattern
+    repl                replace
+
+Options:
+    -h, --help          Show this
+    -v, --version       Show version
+    -d                  only rename directory
+    -f                  only rename file
+    -i                  ignore case
+
+"""
 import os
-import sys
+import re
+
+from docopt import docopt
 
 base_dir = os.getcwd()
 
 
-# print(base_dir)
-
-
-def getfiles(dirpath):
+def getfiles(dirpath, onlyfile=False, onlydir=False):
     # 返回目录下所有文件和目录的名称组成的list, 不包含路径
     files = os.listdir(dirpath)
-    # files = []
-    # for item in os.listdir(dirpath):
-    #     if os.path.isfile(item):
-    #         files.append(item)
-    return files
-
-
-def rename(oldnames):
-    havefile = False
-    for filename in oldnames:
-        if re.match(pattern, filename):
-            havefile = True
-            newname = re.sub(pattern, new, filename)
-            print(filename, '  TO  ', newname)
-    if havefile:
-        exe = input('continue [y/n]?')
-        if exe == 'y':
-            for filename in oldnames:
-                if re.match(pattern, filename):
-                    newname = re.sub(pattern, new, filename)
-                    # print(filename, '  TO  ', newname)
-                    try:
-                        os.rename(filename, newname)
-                    except Exception:
-                        print('rename fail:', filename)
-        else:
-            print('rename cancel.')
+    files.sort()
+    items = []
+    if onlyfile:
+        for i in files:
+            if os.path.isfile(i):
+                items.append(i)
+    elif onlydir:
+        for i in files:
+            if os.path.isdir(i):
+                items.append(i)
     else:
+        items = files
+    return items
+
+
+def ren(items, pattern, repl, flags):
+    renamelist = dict()
+    for oldname in items:
+        newname = re.sub(pattern, repl, oldname, flags=flags)
+        if newname != oldname:
+            # print(oldname, ' \033[31mTO\033[0m ', newname)
+            renamelist[oldname] = newname
+    if not renamelist:
         print('There is no files to rename.')
-    return
-
-
-def ren(namelist):
-    havefile = False
-    for oldname in namelist:
-        newname = re.sub(pattern, new, oldname)
-        if newname == oldname:
-            pass
-        else:
-            print(oldname, ' \033[31mTO\033[0m ', newname)
-            havefile = True or havefile
-    if havefile:
-        dorename = input('\033[36mrename [y/n]?\033[0m').lower()
-        if dorename == 'y':
-            for oldname in namelist:
-                newname = re.sub(pattern, new, oldname)
-                if newname == oldname:
-                    pass
-                else:
-                    try:
-                        os.rename(oldname, newname)
-                    except Exception:
-                        print('\033[33mrename fail:\033[0m', oldname)
-        else:
-            print('nothing changed, exit')
+        return
+    keys = renamelist.keys()
+    maxlength = 0
+    for i in keys:
+        maxlength = max(maxlength, len(i))
+    for k, v in renamelist.items():
+        print('{0:<{maxlength}} \033[31mTO\033[0m {1}'.format(k, v, maxlength=maxlength))
+    action = input('\033[36mRename [y/n]?\033[0m').lower()
+    if action == 'y':
+        for oldname, newname in renamelist.items():
+            try:
+                os.rename(oldname, newname)
+            except:
+                print('Rename fail:', oldname)
     else:
-        print('There is no files to rename.')
+        print('Nothing changed, exit')
+        return
 
 
-if __name__ == "__main__":
-    pattern = re.compile(sys.argv[1])
-    new = sys.argv[2]
-    filenames = getfiles(base_dir)
-    # rename(filenames)
-    ren(filenames)
+if __name__ == '__main__':
+    args = docopt(__doc__, version='1.0')
+    items = getfiles(base_dir, args['-f'], args['-d'])
+    if args['-i']:
+        flags = re.I
+    else:
+        flags = 0
+    ren(items, args['<pattern>'], args['<repl>'], flags)
